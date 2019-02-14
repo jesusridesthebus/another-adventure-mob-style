f009eb3c1932058a58f22e184fa8b93aa643a5f9
emily baier <emilyrbaier@gmail.com>
2019-02-14 14:41:18 -0800

    added images, assigned attributes to radio buttons

--- src/quest/quest.js
+++ src/quest/quest.js
@@ -7,11 +7,15 @@ const questToFind = searchParams.get('id');
 const titleDisplay = document.getElementById('title');
 const descDisplay = document.getElementById('quest-description');
 const choiceDisplay = document.getElementById('quest-choices');
+const imageDisplay = document.getElementById('quest-image');
+const choiceForm = document.getElementById('choice-form');
+const formData = new FormData(choiceForm);
 
 for(let i = 0; i < questChoices.length; i++) {
     const currentChoice = questChoices[i];
     if(currentChoice.id === questToFind) {
         titleDisplay.textContent = currentChoice.title;
+        imageDisplay.src = currentChoice.image;
         descDisplay.textContent = currentChoice.description;
         for(let j = 0; j < currentChoice.choices.length; j++) {
             const userChoice = currentChoice.choices[j];
@@ -21,12 +25,15 @@ for(let i = 0; i < questChoices.length; i++) {
             choiceDisplay.appendChild(input);
             choiceDisplay.appendChild(label);
             input.type = 'radio';
+            input.name = 'choice';
+            input.value = userChoice.value;
             label.textContent = userChoice.description;
-
-            
-
-
         }
     }
 }
 
+choiceForm.addEventListener('submit', function(event){
+    event.preventDefault();
+
+
+});
--- src/quests.js
+++ src/quests.js
@@ -1,19 +1,22 @@
 const assChoice1 = {
     description: 'Run away screaming.',
     hp: 50,
-    gold: 5
+    gold: 5,
+    value: 'assChoice1'
 };
 
 const assChoice2 = {
     description: 'Answer the DIY call of mismatched furniture parts.',
     hp: -5,
-    gold: -50
+    gold: -50,
+    value: 'assChoice2'
 };
 
 const assChoice3 = {
     description: 'Curl up for a nap on the couch with the mystery stain...',
     hp: -50,
-    gold: 0
+    gold: 0,
+    value: 'assChoice3'
 };
 
 const assChoices = [assChoice1, assChoice2, assChoice3];
@@ -21,19 +24,22 @@ const assChoices = [assChoice1, assChoice2, assChoice3];
 const childrenChoice1 = {
     description: 'Jump over the counter to apprehend your child.',
     hp: -5,
-    gold: -25
+    gold: -25,
+    value: 'childrenChoice1'
 };
 
 const childrenChoice2 = {
     description: 'If you love them, let them go...',
     hp: 100,
-    gold: 100
+    gold: 100,
+    value: 'childrenChoice2'
 };
 
 const childrenChoice3 = {
     description: 'SMALAND attendant accuses you of attempted kidnapping, you dramatically defend yourself',
     hp: 12,
-    gold: -100
+    gold: -100,
+    value: 'childrenChoice3'
 };
 
 const childrenChoices = [childrenChoice1, childrenChoice2, childrenChoice3];
@@ -41,19 +47,22 @@ const childrenChoices = [childrenChoice1, childrenChoice2, childrenChoice3];
 const foodChoice1 = {
     description: 'Commit to waiting in line, no matter how long it takes, for the vegan meatballs.',
     hp: -100,
-    gold: -10
+    gold: -10,
+    value: 'foodChoice1'
 };
 
 const foodChoice2 = {
     description: 'Tempted by a hotdog, you decide to head downstairs to the snack bar.',
     hp: 25,
-    gold: -5
+    gold: -5,
+    value: 'foodChoice2'
 };
 
 const foodChoice3 = {
     description: 'Steal meatballs from a distracted parent.',
     hp: 50,
-    gold: 50
+    gold: 50,
+    value: 'foodChoice3'
 };
 
 const foodChoices = [foodChoice1, foodChoice2, foodChoice3];
@@ -62,21 +71,27 @@ const ass = {
     id: 'ass',
     title: 'Enter the As-Is Section',
     description: 'You have entered the deepest, darkest corner of IKEA... The As-Is section. You are overwhelmed with possibilities! Do you...',
-    choices: assChoices
+    choices: assChoices,
+    image: 'assets/ass.jpg'
 };
 
 const children = {
     id: 'children',
-    title: 'Brave SMALLAND',
+    title: 'Brave SMALAND',
     description: 'Your offspring (surprise, you are a parent) is refusing to leave Smaland. You watch them run into the distance. Do you...',
-    choices: childrenChoices
+    choices: childrenChoices,
+    image: 'assets/children.jpg'
+
+
 };
 
 const food = {
     id: 'food',
     title: 'Get Some Meatballs',
     description: 'You enter the food court, the lines are impossibly long. The TV screen flashes something delicious... It\'s meatballs. Do you...',
-    choices: foodChoices
+    choices: foodChoices,
+    image: 'assets/meatballs.jpg'
+
 };
 
 const questChoices = [ass, children, food]; 
